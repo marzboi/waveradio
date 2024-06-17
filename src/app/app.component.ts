eb76e7757333bdab05bcfcebcd269cc854dbe46d
Marco Fratini <marcofratini@outlook.com>
2024-06-17 12:36:08 -0400

    Move main view page location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainViewComponent } from './pages/main-view/main-view.component';
+import { MainViewComponent } from './pages/main-view.component';
 
 @Component({
   selector: 'app-root',
--- src/app/pages/main-view.component.ts
+++ src/app/pages/main-view.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { PlayerComponent } from '../../components/player/player.component';
+import { PlayerComponent } from '../components/player/player.component';
 
 @Component({
   selector: 'app-main-view',
