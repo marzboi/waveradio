ef88d3651c97e4ce4e593a200541238a61b87de2
Marco Fratini <marcofratini@outlook.com>
2024-06-17 12:24:15 -0400

    Update to version 18

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { PlayerComponent } from 'src/app/components/player/player.component';
+import { PlayerComponent } from '../../components/player/player.component';
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
   imports: [CommonModule, PlayerComponent],
   templateUrl: './main-view.component.html',
-  styleUrls: ['./main-view.component.scss'],
+  styleUrl: './main-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainViewComponent {}
